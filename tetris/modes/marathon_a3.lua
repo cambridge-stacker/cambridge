0a0053276bf3d692a79d4420c352e4dd39c64a89
Ishaan Bhardwaj <59454579+SashLilac@users.noreply.github.com>
2020-10-09 21:14:20 -0400

    SG!!!!

--- tetris/modes/marathon_a3.lua
+++ tetris/modes/marathon_a3.lua
@@ -32,6 +32,12 @@ function MarathonA3Game:new()
     
 	self.randomizer = History6RollsRandomizer()
 
+self.SGnames = {
+        "9", "8", "7", "6", "5", "4", "3", "2", "1",
+        "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9",
+        "GM"
+    }
+	
     self.lock_drop = true
     self.lock_hard_drop = true
 	self.enable_hold = true
