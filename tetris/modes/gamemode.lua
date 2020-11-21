7abb8614468fe2f2bad18ab033386fa25614be16
Ishaan Bhardwaj <sashlilac109@gmail.com>
2020-11-21 16:29:24 -0500

    Hard drop can ARE cancel now

--- tetris/modes/gamemode.lua
+++ tetris/modes/gamemode.lua
@@ -271,11 +271,11 @@ function GameMode:processDelays(inputs, ruleset, drop_speed)
 	elseif self.lcd > 0 then
 		self.lcd = self.lcd - 1
 		if ruleset.are_cancel and
-			(self.move == "none" and
+			(self.move == "none" and not self.prev_inputs["up"] and
 			not self.prev_inputs["rotate_left"] and not self.prev_inputs["rotate_left2"] and
 			not self.prev_inputs["rotate_right"] and not self.prev_inputs["rotate_right2"] and
 			not self.prev_inputs["rotate_180"]) and
-			(inputs["left"] or inputs["right"] or
+			(inputs["left"] or inputs["right"] or inputs["up"] or
 			inputs["rotate_left"] or inputs["rotate_left2"] or
 			inputs["rotate_right"] or inputs["rotate_right2"] or
 			inputs["rotate_180"]) then
@@ -292,11 +292,11 @@ function GameMode:processDelays(inputs, ruleset, drop_speed)
 	elseif self.are > 0 then
 		self.are = self.are - 1
 		if ruleset.are_cancel and
-			(self.move == "none" and
+			(self.move == "none" and not self.prev_inputs["up"] and
 			not self.prev_inputs["rotate_left"] and not self.prev_inputs["rotate_left2"] and
 			not self.prev_inputs["rotate_right"] and not self.prev_inputs["rotate_right2"] and
 			not self.prev_inputs["rotate_180"]) and
-			(inputs["left"] or inputs["right"] or
+			(inputs["left"] or inputs["right"] or inputs["up"] or
 			inputs["rotate_left"] or inputs["rotate_left2"] or
 			inputs["rotate_right"] or inputs["rotate_right2"] or
 			inputs["rotate_180"]) then
