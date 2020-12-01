905e4bcc77d7e0d956cb97c09971d3aa181854fd
Ishaan Bhardwaj <sashlilac109@gmail.com>
2020-12-01 11:56:44 -0500

    drawSectionTimesWithSecondary update

--- tetris/modes/gamemode.lua
+++ tetris/modes/gamemode.lua
@@ -519,7 +519,7 @@ function GameMode:drawSectionTimes(current_section)
 	love.graphics.printf(formatTime(self.frames - self.section_start_time), section_x, 40 + 20 * current_section, 90, "left")
 end
 
-function GameMode:drawSectionTimesWithSecondary(current_section)
+function GameMode:drawSectionTimesWithSecondary(current_section, section_colour_function)
 	local section_x = 530
 	local section_secondary_x = 440
 
@@ -530,6 +530,9 @@ function GameMode:drawSectionTimesWithSecondary(current_section)
 	end
 
 	for section, time in pairs(self.secondary_section_times) do
+		if self.section_colour_function then
+			love.graphics.setColor(self:section_colour_function(section))
+		end
 		if section > 0 then
 			love.graphics.printf(formatTime(time), section_secondary_x, 40 + 20 * section, 90, "left")
 		end
