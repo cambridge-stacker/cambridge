e19da98ea10d8bb3dff6a2c37d1342a857007169
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-01-28 21:19:47 -0500

    Standard SRS now has correct amount of move resets

--- tetris/rulesets/standard.lua
+++ tetris/rulesets/standard.lua
@@ -10,6 +10,25 @@ SRS.harddrop_lock = true
 
 SRS.MANIPULATIONS_MAX = 15
 
+function SRS:onPieceDrop(piece, grid)
+	self:checkNewLow(piece)
+	if piece.manipulations >= self.MANIPULATIONS_MAX and piece:isDropBlocked(grid) then
+		piece.locked = true
+	else
+		piece.lock_delay = 0 -- step reset
+	end
+end
+
+function SRS:onPieceMove(piece, grid)
+	piece.lock_delay = 0 -- move reset
+	if piece:isDropBlocked(grid) then
+		piece.manipulations = piece.manipulations + 1
+		if piece.manipulations >= SRS.MANIPULATIONS_MAX then
+			piece.locked = true
+		end
+	end
+end
+
 function SRS:onPieceRotate(piece, grid)
 	piece.lock_delay = 0 -- rotate reset
 	self:checkNewLow(piece)
