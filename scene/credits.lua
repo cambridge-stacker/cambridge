ba235c8a41584ac68828a0aad3b2d159322e369d
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-02-28 18:40:53 -0500

    Credits update <3

--- scene/credits.lua
+++ scene/credits.lua
@@ -30,7 +30,7 @@ function CreditsScene:render()
 
     love.graphics.setFont(font_3x5_4)
     love.graphics.print("Cambridge Credits", 320, 500 - self.frames / 2)
-    love.graphics.print("THANK YOU\nFOR PLAYING!", 320, math.max(1500 - self.frames / 2, 240))
+    love.graphics.print("THANK YOU\nFOR PLAYING!", 320, math.max(1750 - self.frames / 2, 240))
 
     love.graphics.setFont(font_3x5_3)
     love.graphics.print("Game Developers", 320, 550 - self.frames / 2)
@@ -47,8 +47,12 @@ function CreditsScene:render()
         "RocketLanterns\nCylinderKnot\nHammrTime\nKirby703\nMattMayuga\nMyPasswordIsWeak\n" ..
         "Nikki Karissa\noffwo\nsinefuse\nTetro48\nTimmSkiller\nuser74003\nAgentBasey\n" ..
         "CheeZed_Fish\neightsixfivezero\nEricICX\ngizmo4487\nM1ssing0\nMarkGamed7794\n" ..
-        "pokemonfan1937\nSimon\nstratus\nZaptorZap\nThe Absolute PLUS Discord\nTetra Legends Discord\n" ..
-        "Tetra Online Discord\nMultimino Discord\nCambridge Discord\nAnd to you, the player!",
+        "pokemonfan1937\nSimon\nstratus\nZaptorZap\nArchina\nOliver\ncolour_thief\n" ..
+        "Caithness\nkdex\nzid\nsaphie\nSuper302\nAurora\nswitchpalacecorner\nKitaru\n" ..
+        "JBroms\nMany more I definitely missed!\n" ..
+        "The Absolute PLUS Discord\nTetra Legends Discord\nTetra Online Discord\n" ..
+        "Multimino Discord\nHard Drop Discord\nCambridge Discord (R.I.P.)\n" ..
+        "And to you, the player!",
         320, 940 - self.frames / 2
     )
 end
