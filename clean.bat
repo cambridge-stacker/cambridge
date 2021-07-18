c3f6e3451808b3d68fb7af1175dc2e7f747c7550
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-07-17 23:08:01 -0400

    New build scripts for targets other than Windows

--- clean.bat
+++ clean.bat
@@ -7,5 +7,11 @@
 @del dist\win32\SOURCES.md
 @del dist\win32\LICENSE.md
 @rmdir /Q /S dist\win32\libs
+@del dist\other\cambridge.love
+@del dist\other\SOURCES.md
+@del dist\other\LICENSE.md
+@rmdir /Q /S dist\other\libs
+@rmdir /Q /S dist\other
 @del dist\cambridge-windows.zip
 @del dist\cambridge-win32.zip
+@del dist\cambridge-other.zip
