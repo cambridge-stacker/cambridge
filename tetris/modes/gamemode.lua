799a905a9c836960bdf3fe0623c61c653fc5e1f7
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-09-05 22:50:31 -0400

    Remove redundant if

--- tetris/modes/gamemode.lua
+++ tetris/modes/gamemode.lua
@@ -126,9 +126,6 @@ function GameMode:update(inputs, ruleset)
 		if inputs["left"] or inputs["right"] then
 			inputs["up"] = false
 			inputs["down"] = false
-		elseif inputs["up"] or inputs["down"] then
-			inputs["left"] = false
-			inputs["right"] = false
 		end
 	end
 
