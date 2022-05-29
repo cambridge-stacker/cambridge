98d4372455296065687b8f0ce051371a31d8d984
Tetro48 <aymirdanilov6900@gmail.com>
2022-05-29 12:49:05 +0700

    Thread printing change.

--- threaded_replay_code.lua
+++ threaded_replay_code.lua
@@ -14,10 +14,11 @@ replay_load_code = [[
 		dict_ref[value] = key + 1
 		replay_tree[key + 1] = {name = value}
 	end
+	print("Loading replay file list")
 	local replay_file_list = love.filesystem.getDirectoryItems("replays")
 	local binser = require "libs/binser"
 	require "funcs"
-	print("some io")
+	print("Loading replay contents")
 	for i=1, #replay_file_list do
 		local data = love.filesystem.read("replays/"..replay_file_list[i])
 		local new_replay = binser.deserialize(data)[1]
@@ -28,6 +29,7 @@ replay_load_code = [[
 		end
 		table.insert(replay_tree[1], #replays)
 	end
+	print("Sorting replays...")
 	local function padnum(d) return ("%03d%s"):format(#d, d) end
 	table.sort(replay_tree, function(a,b)
 	return tostring(a.name):gsub("%d+",padnum) < tostring(b.name):gsub("%d+",padnum) end)
@@ -36,9 +38,10 @@ replay_load_code = [[
 			return replays[a]["timestamp"] > replays[b]["timestamp"]
 		end)
 	end
-	print("load")
+	print("Pushing replays...")
     love.thread.getChannel( 'replays' ):push( replays )
     love.thread.getChannel( 'replay_tree' ):push( replay_tree )
     love.thread.getChannel( 'dict_ref' ):push( dict_ref )
     love.thread.getChannel( 'loaded_replays' ):push( true )
+	print("Pushed replays.")
 ]]
