ea9f3dd799445b667e3ea4945b820a8351b6648e
Tetro48 <aymirdanilov6900@gmail.com>
2022-10-15 12:30:58 +0700

    Appended "threaded_replay_code.lua" on package scripts.

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r cambridge.love libs load res scene tetris conf.lua main.lua scene.lua funcs.lua
+zip -r cambridge.love libs load res scene tetris conf.lua main.lua scene.lua funcs.lua threaded_replay_code.lua
