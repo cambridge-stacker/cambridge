ab437140f62303d01db4b2e0f6d1943c29491fec
Tetro48 <aymirdanilov6900@gmail.com>
2022-12-13 18:54:41 +0700

    Reduced the executable size further. 1. RPC image files are now no longer included. 2. Max level of compression (9).

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-zip -r cambridge.love libs load res scene tetris conf.lua main.lua scene.lua funcs.lua threaded_replay_code.lua
+zip -r9 cambridge.love libs load res scene tetris conf.lua main.lua scene.lua funcs.lua threaded_replay_code.lua -x "res/img/rpc/*" res/bgm/pacer_test.mp3
