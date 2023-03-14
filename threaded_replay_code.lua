73c2900149724813857399362c75f5a53ccb0828
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2023-03-14 15:00:14 +0700

    Stopped bigint from crashing Cambridge when loading replays.

--- threaded_replay_code.lua
+++ threaded_replay_code.lua
@@ -11,6 +11,20 @@ replay_load_code = [[
 		love.thread.getChannel( 'load_state' ):clear()
 		love.thread.getChannel( 'load_state' ):push(string)
 	end
+	local function toFormattedValue(value)
+	
+		if type(value) == "table" and value.digits and value.sign then
+			local num = ""
+			if value.sign == "-" then
+				num = "-"
+			end
+			for _, digit in pairs(value.digits) do
+				num = num .. math.floor(digit) -- lazy way of getting rid of .0$
+			end
+			return num
+		end
+		return value
+	end
 	setState("Loading replay file list")
 	local replay_file_list = love.filesystem.getDirectoryItems("replays")
 	local binser = require "libs.binser"
@@ -23,6 +37,14 @@ replay_load_code = [[
 			love.filesystem.remove("replays/"..replay_file_list[i])
 			print ("The replay at replays/"..replay_file_list[i].." is corrupted or has no data, thus, deleted.")
 		else
+			for key, value in pairs(new_replay) do
+				new_replay[key] = toFormattedValue(value)
+			end
+			if new_replay.highscore_data then 
+				for key, value in pairs(new_replay.highscore_data) do
+					new_replay.highscore_data[key] = toFormattedValue(value)
+				end
+			end
 			love.thread.getChannel('replay'):push(new_replay)
 		end
 	end
