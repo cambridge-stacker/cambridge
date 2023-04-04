7053e2da14091b18d36bc4f7385965d76d6f01dc
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2023-04-04 15:11:29 +0700

    Updated scripts.

--- clean.bat
+++ clean.bat
@@ -6,12 +6,16 @@
 @del dist\win32\cambridge.exe
 @del dist\win32\SOURCES.md
 @del dist\win32\LICENSE.md
+@del dist\win_aarch64\cambridge.exe
+@del dist\win_aarch64\SOURCES.md
+@del dist\win_aarch64\LICENSE.md
 @rmdir /Q /S dist\win32\libs
 @del dist\other\cambridge.love
 @del dist\other\SOURCES.md
 @del dist\other\LICENSE.md
 @rmdir /Q /S dist\other\libs
 @rmdir /Q /S dist\other
-@del dist\cambridge-windows.zip
-@del dist\cambridge-win32.zip
-@del dist\cambridge-other.zip
+@del dist\cambridge_windows_x64.zip
+@del dist\cambridge_windows_aarch64_experimental.zip
+@del dist\cambridge_windows_x86.zip
+@del dist\cambridge_other.zip
--- release.bat
+++ release.bat
@@ -10,27 +10,34 @@ mkdir dist\other\libs
 
 copy /b dist\windows\love.exe+cambridge.love dist\windows\cambridge.exe
 copy /b dist\win32\love.exe+cambridge.love dist\win32\cambridge.exe
+copy /b dist\win_aarch64\love.exe+cambridge.love dist\win_aarch64\cambridge.exe
 copy /b cambridge.love dist\other\cambridge.love
 
-copy libs\discord-rpc.dll dist\windows\libs
-copy libs\discord-rpc.dll dist\win32\libs
-copy libs\discord-rpc.* dist\other\libs
+copy libs\discord-rpc_x64.dll dist\windows\libs
+copy libs\discord-rpc_x86.dll dist\win32\libs
+copy libs\discord-rpc* dist\other\libs
 
 copy SOURCES.md dist\windows
 copy LICENSE.md dist\windows
 copy SOURCES.md dist\win32
 copy LICENSE.md dist\win32
+copy SOURCES.md dist\win_aarch64
+copy LICENSE.md dist\win_aarch64
 copy SOURCES.md dist\other
 copy LICENSE.md dist\other
 
 cd dist\windows
-tar -a -c -f ..\cambridge-windows.zip cambridge.exe *.dll libs *.md
+tar -acf ..\cambridge_windows_x64.zip cambridge.exe *.dll libs *.md
+cd ..\..
+
+cd dist\win_aarch64
+tar -acf ..\cambridge_windows_aarch64_experimental.zip cambridge.exe *.dll libs *.md
 cd ..\..
 
 cd dist\win32
-tar -a -c -f ..\cambridge-win32.zip cambridge.exe *.dll libs *.md
+tar -acf ..\cambridge_windows_x86.zip cambridge.exe *.dll libs *.md
 cd ..\..
 
 cd dist\other
-tar -a -c -f ..\cambridge-other.zip cambridge.love libs *.md
+tar -acf ..\cambridge_other.zip cambridge.love libs *.md
 cd ..\..
