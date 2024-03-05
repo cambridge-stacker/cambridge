6027f5038ca578166ef6dd2cfc818b02402ff954
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2024-03-05 10:44:04 +0700

    Split most of module handling into a separate file

--- main.lua
+++ main.lua
@@ -19,6 +19,7 @@ function love.load()
 	require "load.bgm"
 	require "load.save"
 	require "load.bigint"
+	require "load.modules"
 	require "load.version"
 	loadSave()
 	require "funcs"
@@ -50,97 +51,6 @@ function love.load()
 	-- this is executed after the sound table is generated. why is that is unknown.
 	if config.secret then playSE("welcome") end
 end
----@param tbl table
----@param directory string
----@param blacklisted_string string
-function recursivelyLoadRequireFileTable(tbl, directory, blacklisted_string)
-	--LOVE 12.0 will warn about require strings having forward slashes in them if this is not done.
-	local require_string = string.gsub(directory, "/", ".")
-	local list = love.filesystem.getDirectoryItems(directory)
-	for index, name in ipairs(list) do
-		
-		if love.filesystem.getInfo(directory.."/"..name, "directory") then
-			tbl[#tbl+1] = {name = name, is_directory = true}
-			recursivelyLoadRequireFileTable(tbl[#tbl], directory.."/"..name, blacklisted_string)
-		end
-		if name ~= blacklisted_string and name:sub(-4) == ".lua" then
-			tbl[#tbl+1] = require(require_string.."."..name:sub(1, -5))
-			if not (type(tbl[#tbl]) == "table" and type(tbl[#tbl].__call) == "function") then
-				error("Add a return to "..directory.."/"..name..".\nMust be a table with __call function.", 1)
-			end
-		end
-	end
-end
-
-function unloadModules()
-	--module reload.
-	for key, value in pairs(package.loaded) do
-		if string.sub(key, 1, 7) == "tetris." then
-			package.loaded[key] = nil
-		end
-	end
-end
-
----@param init table
-function recursivelyTagModules(init, tbl, tag_tbl)
-	if not tbl then tbl = init end
-	if not tag_tbl then tag_tbl = {} end
-	for k, v in pairs(tbl) do
-		if type(v) == "table" and v.is_directory == true and not (v.is_tag) then
-			recursivelyTagModules(init, v, tag_tbl)
-		end
-		if type(v) == "table" and type(v.tags) == "table" then
-			for k2, v2 in pairs(v.tags) do
-				tag_tbl[v2] = tag_tbl[v2] or {name = v2, is_directory = true, is_tag = true}
-				table.insert(tag_tbl[v2], v)
-			end
-		end
-	end
-	if init ~= tbl then return end
-
-	local sorted_tags = {}
-	--#region Sort tag names
-	for key, value in pairs(tag_tbl) do
-		table.insert(sorted_tags, value)
-	end
-	local function padnum(d) return ("%03d%s"):format(#d, d) end
-	table.sort(sorted_tags, function(a,b)
-	return tostring(a.name):gsub("%d+",padnum) < tostring(b.name):gsub("%d+",padnum) end)
-	--#endregion
-
-	for key, value in ipairs(sorted_tags) do
-		table.insert(init, value)
-	end
-end
-
-function initModules()
-	game_modes = {}
-	recursivelyLoadRequireFileTable(game_modes, "tetris/modes", "gamemode.lua")
-	-- mode_list = love.filesystem.getDirectoryItems("tetris/modes")
-	-- for i=1,#mode_list do
-	-- 	if(mode_list[i] ~= "gamemode.lua" and string.sub(mode_list[i], -4) == ".lua") then
-	-- 		game_modes[#game_modes+1] = require ("tetris.modes."..string.sub(mode_list[i],1,-5))
-	-- 	end
-	-- end
-	rulesets = {}
-	recursivelyLoadRequireFileTable(rulesets, "tetris/rulesets", "ruleset.lua")
-	-- rule_list = love.filesystem.getDirectoryItems("tetris/rulesets")
-	-- for i=1,#rule_list do
-	-- 	if(rule_list[i] ~= "ruleset.lua" and string.sub(rule_list[i], -4) == ".lua") then
-	-- 		rulesets[#rulesets+1] = require ("tetris.rulesets."..string.sub(rule_list[i],1,-5))
-	-- 	end
-	-- end
-
-	--sort mode/rule lists
-	local function padnum(d) return ("%03d%s"):format(#d, d) end
-	table.sort(game_modes, function(a,b)
-	return tostring(a.name):gsub("%d+",padnum) < tostring(b.name):gsub("%d+",padnum) end)
-	table.sort(rulesets, function(a,b)
-	return tostring(a.name):gsub("%d+",padnum) < tostring(b.name):gsub("%d+",padnum) end)
-	recursivelyTagModules(game_modes)
-	recursivelyTagModules(rulesets)
-end
-
 
 local io_thread
 
