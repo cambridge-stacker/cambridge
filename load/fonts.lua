c72690a780557715095e111eb6fd68d075330241
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2024-07-11 14:59:02 +0700

    refactor: Deduplicated the glyph strings on font loading

--- load/fonts.lua
+++ load/fonts.lua
@@ -1,28 +1,28 @@
+local font_3x5_glyphs = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" ..
+	"`abcdefghijklmnopqrstuvwxyz{|}~™АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+local font_8x11_glyphs = " 0123456789:;.,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!/\\^@$%<=>()*-+[]_&"
+
 font_3x5 = love.graphics.newImageFont(
 	"res/fonts/3x5-ext.png",
-	" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" ..
-	"`abcdefghijklmnopqrstuvwxyz{|}~™АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+	font_3x5_glyphs,
 	-1
 )
 
 font_3x5_2 = love.graphics.newImageFont(
 	"res/fonts/3x5_double-ext.png",
-	" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" ..
-	"`abcdefghijklmnopqrstuvwxyz{|}~™АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+	font_3x5_glyphs,
 	-2
 )
 
 font_3x5_3 = love.graphics.newImageFont(
 	"res/fonts/3x5_medium-ext.png",
-	" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" ..
-	"`abcdefghijklmnopqrstuvwxyz{|}~™АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+	font_3x5_glyphs,
 	-3
 )
 
 font_3x5_4 = love.graphics.newImageFont(
 	"res/fonts/3x5_large-ext.png",
-	" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" ..
-	"`abcdefghijklmnopqrstuvwxyz{|}~™АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя",
+	font_3x5_glyphs,
 	-4
 )
 
@@ -30,14 +30,12 @@ font_3x5_4 = love.graphics.newImageFont(
 -- but that would break compatibility :(
 font_8x11_small = love.graphics.newImageFont(
 	"res/fonts/8x11.png",
-	" 0123456789:;.,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ..
-	"?!/\\^@$%<=>()*-+[]_&",
+	font_8x11_glyphs,
 	1
 )
 
 font_8x11 = love.graphics.newImageFont(
 	"res/fonts/8x11_medium.png",
-	" 0123456789:;.,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" ..
-	"?!/\\^@$%<=>()*-+[]_&",
+	font_8x11_glyphs,
 	1
 )
