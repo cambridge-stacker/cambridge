4a9daccfcc206dd5b40fb14c8fed32610f635249
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-04-30 10:00:27 +0700

    fix: pass all arguments from start.bat to the respective love executable

--- start.bat
+++ start.bat
@@ -6,5 +6,5 @@ setlocal
 rem This solution of detecting the current CPU taken from here: https://stackoverflow.com/a/24590583
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set CURRENT_CPU=32BIT || set CURRENT_CPU=64BIT
 
-if %CURRENT_CPU%==32BIT .\dist\win32\love.exe .
-if %CURRENT_CPU%==64BIT .\dist\windows\love.exe .
+if %CURRENT_CPU%==32BIT .\dist\win32\love.exe . %*
+if %CURRENT_CPU%==64BIT .\dist\windows\love.exe . %*
