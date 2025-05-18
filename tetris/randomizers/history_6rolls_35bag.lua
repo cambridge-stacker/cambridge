a8bbaf78017be83aaa587aef2e7c3fcdc30d690c
zaphod77 <zaphod@tdl.com>
2025-05-18 09:39:37 -0500

    Update history_6rolls_35bag.lua
    corrected error in genral algorithm, and implemented original TI bug.

--- tetris/randomizers/history_6rolls_35bag.lua
+++ tetris/randomizers/history_6rolls_35bag.lua
@@ -14,14 +14,14 @@ function History6Rolls35PoolRandomizer:initialize()
 		"Z", "Z", "Z", "Z", "Z",
 		"O", "O", "O", "O", "O",
 	}
-	self.droughts = {
-		I = 0,
-		T = 0,
-		L = 0,
-		J = 0,
-		S = 0,
-		Z = 0,
-		O = 0,
+	self.droughts = {  -- don't ask me why TI does this, bit it does. :)
+		I = 4,
+		T = 4,
+		L = 4,
+		J = 4,
+		S = 4,
+		Z = 4,
+		O = 4,
 	}
 	self.piece_index = {
 		"I",
@@ -35,42 +35,83 @@ function History6Rolls35PoolRandomizer:initialize()
 end
 
 function History6Rolls35PoolRandomizer:generatePiece()
-	local index, x
+	local index, x, highscore, didreroll, didfirst
+	didreroll = false -- did we reroll
+	didfirst = false -- did we just do the first piece
 	if self.first then
-		index = love.math.random(20)
-		x = self.pool[index]
+		index = love.math.random(20) -- hack to pick non S Z O first try every time.
+		x = self.pool[index]	     -- not actually how thwe real thing works, but close enough.
 		self.first = false
+		didfirst = true
 	else
-		for i = 1, 6 do
+		for i = 1, 6 do			 
 			index = love.math.random(#self.pool)
 			x = self.pool[index]
-			if not self:inHistory(x) or i == 6 then
+			if not self:inHistory(x)
 				break
 			end
+			didrerool=true -- checked leater
+			self.pool[index]=self.GetMostDroughtedPiece()   -- update the bag
+			index = love.math.random(#self.pool)		-- reroll in case we are about to fall out
+			x = self.pool[index]				-- yes, this burns an extra number from the rng most of the time.
 		end
 	end
-	self.pool[index] = self:updateHistory(x)
+	highscore=self.CheckHighDroughtCount()  -- check drought count before updating histogram so we can implement the bug
+	self.UpdateHistory(x)			--  update the history even on first piece
+	self.UpdateHistogram(x)			-- update the histogram even on first piece
+	-- we only update the bag sometimes, due to a bug in TI
+	if didfirst then
+		return x -- don't update for first piece, skip the other two tests. this is not the bug, as the first piece was not drawn from the bag.
+	end
+	-- we should always update the bag here, but we only update it in two cases.
+	if highscore < self.CheckHighDroughtCount() then
+		self.pool[index]=self.GetMostDroughtedPiece()  -- do update if the high drought count went up
+	end
+	if not didreroll
+		self.pool[index]=self.GetMostDroughtedPiece() -- do update if there was no reroll.
+	end
+	-- if neither happened, the bag does NOT get updated now. to remove the bug, comment ouut both ifs and one of the updates above, so the bag always updates except for first piece
 	return x
 end
 
 function History6Rolls35PoolRandomizer:updateHistory(shape)
 	table.remove(self.history, 1)
 	table.insert(self.history, shape)
+end
+function History6Rolls35PoolRandomizer:CheckHighDroughtCount()
 
 	local highdrought
 	local highdroughtcount = 0
+	for k, v in pairs(self.piece_index) do
+			if self.droughts[v] >= highdroughtcount then
+				highdrought = v
+				highdroughtcount = self.droughts[v]
+			end
+	end
+	return highdroughtcount
+end
+function History6Rolls35PoolRandomizer:GetMostDroughtedPiece()
+
+	local highdrought
+	local highdroughtcount = 0
+	for k, v in pairs(self.piece_index) do
+			if self.droughts[v] >= highdroughtcount then
+				highdrought = v
+				highdroughtcount = self.droughts[v]
+			end
+	end
+	return highdrought
+end
+function History6Rolls35PoolRandomizer:UpdateHistogram(shape)
+
 	for k, v in pairs(self.piece_index) do
 		if v == shape then
 			self.droughts[v] = 0
 		else
 			self.droughts[v] = self.droughts[v] + 1
-			if self.droughts[v] >= highdroughtcount then
-				highdrought = v
-				highdroughtcount = self.droughts[v]
-			end
 		end
 	end
-	return highdrought
+	return true
 end
 
 function History6Rolls35PoolRandomizer:inHistory(piece)
