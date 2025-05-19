05fc73c5bcda5fa21a4793ef5641af6165e38d5c
zaphod77 <zaphod@tdl.com>
2025-05-19 11:32:51 -0500

    Update history_6rolls_35bag.lua
    Style fixes, and one typo.

--- tetris/randomizers/history_6rolls_35bag.lua
+++ tetris/randomizers/history_6rolls_35bag.lua
@@ -35,14 +35,14 @@ function History6Rolls35PoolRandomizer:initialize()
 end
 
 function History6Rolls35PoolRandomizer:generatePiece()
-	local index, x, highscore, didreroll, didfirst
-	didreroll = false -- did we reroll
-	didfirst = false -- did we just do the first piece
+	local index, x, highscore, did_reroll, did_first
+	did_reroll = false -- did we reroll
+	did_first = false -- did we just do the first piece
 	if self.first then
 		index = love.math.random(20) -- hack to pick non S Z O first try every time.
 		x = self.pool[index]	     -- not actually how the real thing works, but close enough.
 		self.first = false
-		didfirst = true
+		did_first = true
 	else
 		for i = 1, 6 do			 
 			index = love.math.random(#self.pool)
@@ -50,59 +50,59 @@ function History6Rolls35PoolRandomizer:generatePiece()
 			if not self:inHistory(x) then
 				break
 			end
-			didrerool=true -- checked later
-			self.pool[index]=self:GetMostDroughtedPiece()   -- update the bag
+			did_reroll=true -- checked later
+			self.pool[index]=self:getMostDroughtedPiece()   -- update the bag
 			index = love.math.random(#self.pool)		-- reroll in case we are about to fall out
 			x = self.pool[index]				-- yes, this burns an extra number from the rng most of the time.
 		end
 	end
-	highscore=self:CheckHighDroughtCount()  -- check drought count before updating histogram so we can implement the bug
-	self:UpdateHistory(x)			--  update the history even on first piece
-	self:UpdateHistogram(x)			-- update the histogram even on first piece
+	highscore=self:checkHighDroughtCount()  -- check drought count before updating histogram so we can implement the bug
+	self:updateHistory(x)			--  update the history even on first piece
+	self:updateHistogram(x)			-- update the histogram even on first piece
 	-- we only update the bag sometimes, due to a bug in TI
-	if didfirst then
+	if did_first then
 		return x -- don't update for first piece, skip the other two tests. this is not the bug, as the first piece was not drawn from the bag.
 	end
 	-- we should always update the bag here, but we only update it in two cases.
-	if highscore < self:CheckHighDroughtCount() then
-		self.pool[index]=self:GetMostDroughtedPiece()  -- do update if the high drought count went up
+	if highscore < self:checkHighDroughtCount() then
+		self.pool[index]=self:getMostDroughtedPiece()  -- do update if the high drought count went up
 	end
-	if not didreroll then
-		self.pool[index]=self:GetMostDroughtedPiece() -- do update if there was no reroll.
+	if not did_reroll then
+		self.pool[index]=self:getMostDroughtedPiece() -- do update if there was no reroll.
 	end
 	-- if neither happened, the bag does NOT get updated now. to remove the bug, comment ouut both ifs and one of the updates above, so the bag always updates except for first piece
 	return x
 end
 
-function History6Rolls35PoolRandomizer:UpdateHistory(shape)
+function History6Rolls35PoolRandomizer:updateHistory(shape)
 	table.remove(self.history, 1)
 	table.insert(self.history, shape)
 end
-function History6Rolls35PoolRandomizer:CheckHighDroughtCount()
+function History6Rolls35PoolRandomizer:checkHighDroughtCount()
 
-	local highdrought
-	local highdroughtcount = 0
+	local high_drought
+	local high_drought_count = 0
 	for k, v in pairs(self.piece_index) do
-			if self.droughts[v] >= highdroughtcount then
-				highdrought = v
-				highdroughtcount = self.droughts[v]
+			if self.droughts[v] >= high_drought_count then
+				high_drought = v
+				high_drought_count = self.droughts[v]
 			end
 	end
-	return highdroughtcount
+	return highd_rought_count
 end
-function History6Rolls35PoolRandomizer:GetMostDroughtedPiece()
+function History6Rolls35PoolRandomizer:getMostDroughtedPiece()
 
-	local highdrought 
-	local highdroughtcount = 0
+	local high_drought 
+	local high_drought_count = 0
 	for k, v in pairs(self.piece_index) do
-			if self.droughts[v] >= highdroughtcount then
-				highdrought = v
-				highdroughtcount = self.droughts[v]
+			if self.droughts[v] >= high_drought_count then
+				high_drought = v
+				high_droughtcount = self.droughts[v]
 			end
 	end
-	return highdrought
+	return high_drought
 end
-function History6Rolls35PoolRandomizer:UpdateHistogram(shape)
+function History6Rolls35PoolRandomizer:updateHistogram(shape)
 
 	for k, v in pairs(self.piece_index) do
 		if v == shape then
