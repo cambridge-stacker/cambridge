59cbb70b2d8994ac6f5f2338cff496556c1cc9bc
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-06-26 17:05:08 +0200

    added Gimmick to Survival A3

--- tetris/modes/survival_a3.lua
+++ tetris/modes/survival_a3.lua
@@ -10,7 +10,7 @@ local SurvivalA3Game = GameMode:extend()
 SurvivalA3Game.name = "Survival A3"
 SurvivalA3Game.hash = "SurvivalA3"
 SurvivalA3Game.tagline = "The blocks turn black and white! Can you make it to level 1300?"
-SurvivalA3Game.tags = {"Survival", "Arika", "20G Start"}
+SurvivalA3Game.tags = {"Survival", "Arika", "20G Start", "Gimmick"}
 
 function SurvivalA3Game:new(secret_inputs)
 	SurvivalA3Game.super:new(secret_inputs)
