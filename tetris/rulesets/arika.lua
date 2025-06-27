c69a7e8870a5b900a743e35b74ced6a4ae405223
Tetro48 <76738929+Tetro48@users.noreply.github.com>
2025-06-27 18:59:20 +0700

    fix: fix description of arika.lua

--- tetris/rulesets/arika.lua
+++ tetris/rulesets/arika.lua
@@ -5,7 +5,7 @@ local ARS = Ruleset:extend()
 
 ARS.name = "Classic ARS"
 ARS.hash = "Arika"
-ARS.description = "Akira Rotation System, from TGM1 and TGM2/TAP!"
+ARS.description = "Arika Rotation System, from TGM1 and TGM2/TAP!"
 
 ARS.spawn_positions = {
 	I = { x=5, y=4 },
