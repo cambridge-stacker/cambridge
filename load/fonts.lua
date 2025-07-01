5998345f1da278877c36e535ab0db09408888964
Mizu <40831463+Rexxt@users.noreply.github.com>
2025-07-01 19:58:04 +0200

    large font update

--- load/fonts.lua
+++ load/fonts.lua
@@ -1,13 +1,17 @@
 local font_3x5_glyphs = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_" ..
 	"`abcdefghijklmnopqrstuvwxyz{|}~™АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя←↓↑→"
-local font_8x11_glyphs = " 0123456789:;.,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!/\\^@$%<=>()*-+[]_&"
+local font_8x11_glyphs = " 0123456789:;.,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?!/\\^@$%<=>()*-+[]_&⬅⬇⬆➡"
 
 -- A collection of some special characters.
 chars = {
-	left = '←',
-	down = '↓',
-	up = '↑',
-	right = '→',
+	small_left = '←',
+	small_down = '↓',
+	small_up = '↑',
+	small_right = '→',
+	big_left = '⬅',
+	big_down = '⬇',
+	big_up = '⬆',
+	big_right = '➡',
 }
 
 custom_fonts = {}
